a6fcb7bc41885d3ba71088a6d987bb570307aa57
SteenMaxwell <steen.maxwell@gmail.com>
2024-11-12 20:05:51 +0000

    Adds correct spacings to code

--- data_transform.py
+++ data_transform.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
+
 class DataTransform:
     def __init__(self, df):
         self.df = df
+
 
     def date_conversion_to_datetime(self, date_columns):
         for col in date_columns:
@@ -13,11 +15,13 @@ class DataTransform:
     #    self.df['term'] = self.df['term'].str.extract(r'(\d+)').astype('Int64')
     #    return self.df
     
+
     def categorical_conversion(self, columns):
         for col in columns:
             self.df[col] = self.df[col].astype('category')
         return self.df
     
+
     def set_categorical_columns(self):
         categorical_columns = [
             'grade', 'sub_grade', 'employment_length', 'home_ownership', 
@@ -27,6 +31,7 @@ class DataTransform:
         
         self.categorical_conversion(categorical_columns)
         return self.df
+    
     
     def transform(self):
         self.date_conversion_to_datetime(['issue_date', 'earliest_credit_line', 'last_payment_date', 'next_payment_date', 'last_credit_pull_date'])

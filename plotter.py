f2300f7d5457793ef8417e5317453bdf0fbb4ce5
SteenMaxwell <steen.maxwell@gmail.com>
2024-11-14 15:29:18 +0000

    Adds view_correlation method to Plotter class

--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Plotter:
@@ -49,16 +50,39 @@ class Plotter:
         plt.show()
 
 
-    def view_outliers(self, columns, cols=3):
-        rows = (len(columns) + cols -1) // cols
+    def view_outliers(self, columns=None, cols=3):
+    
+        if columns is None:
+            columns = self.df.select_dtypes(include='number').columns
+        
+        numeric_columns = [col for col in columns if col in self.df.select_dtypes(include='number').columns]
+        
+        rows = (len(numeric_columns) + cols -1) // cols
         fig, axes = plt.subplots(rows, cols, figsize=(6 * cols, 5 * rows))
         axes = axes.flatten()
 
-        for i, col in enumerate(columns):
+        for i, col in enumerate(numeric_columns):
             sns.boxplot(y=self.df[col], color='lightgreen', ax=axes[i], showfliers=True)
 
             axes[i].set_title(f'Box plot for {col}')
             axes[i].set_ylabel(col)
 
         plt.tight_layout()
+        plt.show()
+
+    
+    def view_correlation(self):
+        numeric_df = self.df.select_dtypes(include=np.number)
+        
+        corr = numeric_df.corr()
+
+        mask = np.zeros_like(corr, dtype=np.bool_)
+        mask[np.triu_indices_from(mask)] = True
+
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+        sns.heatmap(corr, mask=mask, square=True, linewidths=.5, annot=True, annot_kws={'size': 8}, fmt='.2f', cmap=cmap)
+        plt.yticks(rotation=0, fontsize=8)
+        plt.xticks(fontsize=8)
+        plt.title('Correlation matrix of all Numerical variables')
         plt.show()

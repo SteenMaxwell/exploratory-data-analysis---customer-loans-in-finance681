3a95b7fae29fa27bd078af83e25b556452d1625d
SteenMaxwell <steen.maxwell@gmail.com>
2024-11-22 11:33:30 +0000

    Adds correct format and docstrings to script

--- data_frame_info.py
+++ data_frame_info.py
@@ -1,10 +1,37 @@
-import pandas as pd
-
-
 class DataFrameInfo:
+    '''
+    DataFrameInfo class has a range of methods for extracting information out of 
+    a DataFrame. Use this class to gain information about the data.
+
+    Methods:
+    -------
+    dtype_for_columns()
+        returns dtype for each column in a DataFrame.
+
+    shape_of_data()
+        returns the shape of the DataFrame.
+
+    describe_columns()
+        returns a description of each column in a DataFrame.
+
+    extract_statistical_values()
+        Extracts and returns mean, median and standard deviation for numeric columns.
+
+    distinct_values_categorical_count()
+        Returns the distinct number of unique items in a categorical columns.
+
+    null_value_counts()
+        Returns either the null count as a percentage or a number.
+    '''
     def __init__(self, df):
+        '''
+        Attributes:
+        ----------
+        df: pd.DataFrame
+            The DataFrame containing the dataset to be analysed and visualised.
+        '''
         self.df = df
-    
+
 
     def dtype_for_columns(self):
         '''Returns the dtype for each column in the DataFrame.'''
@@ -13,13 +40,25 @@ class DataFrameInfo:
 
 
     def shape_of_data(self):
-        '''Returns the shape of the data: a tuple (rows, columns).'''
+        '''Returns the shape of the data; a tuple (rows, columns).'''
         shape = self.df.shape
         return shape
 
 
-    def describe_columns(self, columns):
-        '''Returns a description of each column in a DataFrame.'''
+    def describe_columns(self, columns: list):
+        '''Returns a description of each column in a DataFrame.
+        
+        Parameter:
+        ----------
+        columns: list of str
+        A list of column names in the DataFrame to describe.
+        
+        Returns:
+        -------
+        described_columns: dict
+            A dictionary where each key is a column name, and the value is a description 
+            of that column.
+        '''
         described_columns = {}
         for col in columns:
             described_columns[col] = self.df[col].describe()
@@ -41,19 +80,44 @@ class DataFrameInfo:
         '''Returns the distinct number of unique items in the categorical columns.'''
         categorical_columns = self.df.select_dtypes(include='category').columns
         distinct_counts = {col: self.df[col].nunique() for col in categorical_columns}
+
         return distinct_counts
 
 
     def null_value_count(self, as_percentage=False):
         '''Returns either the null count as a percentage or a number depending on the argument used.
+
            Parameter:
            ---------
-           as_percentage=False : so can decide if method should return a percentage or number
+           as_percentage=False: so can decide if method should return a percentage or number
                 - For a percentage, include 'as_percentage = True' as an argument
                 - For the sum number, no argument needed
         '''
         if as_percentage:
-            null_counts = self.df.isnull().sum()/len(self.df) * 100  
+            null_counts = self.df.isnull().sum()/len(self.df) * 100
         else:
-            null_counts = self.df.isnull().sum()  
+            null_counts = self.df.isnull().sum()
         return null_counts
+
+
+    def skew_check(self, threshold=1):
+        '''
+        Identifies skewed columns based on a given skewness threshold.
+
+        Parameter:
+        ----------
+        threshold : float or int, default=1
+            The skewness value threshold. Columns with an absolute skewness
+            greater than this threshold are identified as skewed and added 
+            to the output list.
+    
+        Returns:
+        -------
+        skewed_columns : Index
+            Index of column names in the DataFrame that have skewness values 
+            exceeding the specified threshold.
+        '''
+        numeric_df = self.df.select_dtypes(include=['number'])
+        skewness = numeric_df.skew()
+        skewed_columns = skewness[abs(skewness) > threshold].index
+        return skewed_columns

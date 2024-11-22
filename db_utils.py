71cc41bcb3d957d3787384a79f723ef8f7175a74
SteenMaxwell <steen.maxwell@gmail.com>
2024-11-22 11:31:00 +0000

    Adds correct structure and docstrings to script

--- db_utils.py
+++ db_utils.py
@@ -1,3 +1,8 @@
+'''
+Imports necessary for loading YAML configuration files, handling data frames
+with pandas, and creating database connections using SQLAlchemy.
+'''
+
 from yaml import safe_load
 import pandas as pd
 from sqlalchemy import create_engine
@@ -5,21 +10,23 @@ from sqlalchemy import create_engine
 
 def loading_credentials():
     '''
-    This function loads the credentials from a YAML file for the AWS RDS so the dataset can be accessed locally.
+    This function loads the credentials from a YAML file
+    for the AWS RDS so the dataset can be accessed locally.
 
     Returns:
     -------
     returns the credentials given in the credentials.yaml file.
     '''
-    
-    with open('credentials.yaml', 'r') as file:
+
+    with open('credentials.yaml', 'r', encoding='utf-8') as file:
         credentials = safe_load(file)
     return credentials
 
 
 class RDSDatabaseConnector:
     '''
-    Used to ultimately access an AWS RDS database and save the 'loan_payments' table to a CSV file so it can be accessed locally.
+    Used to ultimately access an AWS RDS database and save the 'loan_payments' 
+    table to a CSV file so it can be accessed locally.
 
     Methods:
     -------
@@ -58,8 +65,9 @@ class RDSDatabaseConnector:
         self.password = credentials.get('RDS_PASSWORD')
         self.database = credentials.get('RDS_DATABASE')
         self.port = credentials.get('RDS_PORT')
+        self.engine = None
 
-    
+
     def initialise_engine(self):
         '''
         This method initialises a SQLAlchemy engine using the credentials given. 
@@ -71,15 +79,16 @@ class RDSDatabaseConnector:
 
     def extract_data(self):
         '''
-        This method extracts the dataset 'loan_payments' from the AWS RDS database and returns it as a pandas DataFrame.
+        This method extracts the dataset 'loan_payments' from the AWS RDS database 
+        and returns it as a pandas DataFrame.
 
         Returns:
         -------
         returns a pandas DataFrame of the 'loan_payments' table from the RDS database.
         '''
 
-        loan_payments_df = pd.read_sql_table('loan_payments', self.engine)
-        return loan_payments_df
+        extracted_df = pd.read_sql_table('loan_payments', self.engine)
+        return extracted_df
 
 
 def save_data_csv(df):
@@ -107,9 +116,9 @@ def loading_data():
     df = pd.read_csv('loan_payments_data.csv')
     return df
 
-
-credentials_needed = loading_credentials()
-connector = RDSDatabaseConnector(credentials_needed)
-connector.initialise_engine()
-loan_payments_df = connector.extract_data()
-save_data_csv(loan_payments_df)
+if __name__ == '__main__':
+    credentials_needed = loading_credentials()
+    connector = RDSDatabaseConnector(credentials_needed)
+    connector.initialise_engine()
+    loan_payments_df = connector.extract_data()
+    save_data_csv(loan_payments_df)
